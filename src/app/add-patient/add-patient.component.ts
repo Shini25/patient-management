752a5be598aeaffbe3cb40981afc9345aea93481
shini25 <lisback25@gmail.com>
2024-06-08 19:02:14 +0300

    andeha hikaly @7

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PatientService } from '../services/patient.service';
 import { ToastrService } from 'ngx-toastr';
@@ -16,7 +16,7 @@ declare global {
   templateUrl: './add-patient.component.html',
   styleUrls: ['./add-patient.component.css'],
 })
-export class AddPatientComponent {
+export class AddPatientComponent implements OnInit {
 
   @ViewChild('newPatientElement') newPatientElement: ElementRef | undefined;
 
@@ -85,6 +85,49 @@ export class AddPatientComponent {
           this.toastr.success('Patient added successfully!');
         });
       }
+    }
+  }
+
+
+
+  phrases: string[] = [
+    "Welcome to patient registration!",
+    "All patients must come here before ",
+    "visiting the doctor's office."
+    
+  ];
+  displayedText: string = '';
+  currentPhraseIndex: number = 0;
+  currentCharIndex: number = 0;
+  isDeleting: boolean = false;
+  typingSpeed: number = 150;
+  deletingSpeed: number = 50;
+  pauseBeforeDelete: number = 2000;
+  pauseBetweenPhrases: number = 1000;
+
+
+  ngOnInit(): void {
+    this.type();
+  }
+
+  type() {
+    const currentPhrase = this.phrases[this.currentPhraseIndex];
+
+    if (!this.isDeleting && this.currentCharIndex < currentPhrase.length) {
+      this.displayedText = currentPhrase.substring(0, this.currentCharIndex + 1);
+      this.currentCharIndex++;
+      setTimeout(() => this.type(), this.typingSpeed);
+    } else if (this.isDeleting && this.currentCharIndex > 0) {
+      this.displayedText = currentPhrase.substring(0, this.currentCharIndex - 1);
+      this.currentCharIndex--;
+      setTimeout(() => this.type(), this.deletingSpeed);
+    } else if (!this.isDeleting && this.currentCharIndex === currentPhrase.length) {
+      this.isDeleting = true;
+      setTimeout(() => this.type(), this.pauseBeforeDelete);
+    } else if (this.isDeleting && this.currentCharIndex === 0) {
+      this.isDeleting = false;
+      this.currentPhraseIndex = (this.currentPhraseIndex + 1) % this.phrases.length;
+      setTimeout(() => this.type(), this.pauseBetweenPhrases);
     }
   }
 }
